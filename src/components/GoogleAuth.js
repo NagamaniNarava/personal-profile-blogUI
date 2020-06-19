4ea2343329643ac8be03e43d42720155897a9a51
NagamaniNarava <narava.nagamani565@gmail.com>
2020-06-18 18:26:31 -0700

    Added Google OAuth clientId

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -8,7 +8,7 @@ class GoogleAuth extends React.Component {
       window.gapi.client
         .init({
           clientId:
-            '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
+            '886664829453-al2hr58lk7vjfep005erjfbndsun0vn1.apps.googleusercontent.com',
           scope: 'email'
         })
         .then(() => {
