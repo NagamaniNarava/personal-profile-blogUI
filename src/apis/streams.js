3b3a19c03d66066e815ba1dd90c18e31786b34d1
NagamaniNarava <narava.nagamani565@gmail.com>
2020-07-01 14:05:03 -0700

    beanstalk url change

--- src/apis/streams.js
+++ src/apis/streams.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://personalprofileblogsping-env.eba-5mqfpmi2.us-east-2.elasticbeanstalk.com/api'
+  baseURL: 'http://Personalprofileblogsping-env.eba-vyzz4uzw.us-east-2.elasticbeanstalk.com/api'
   //baseURL: 'http://localhost:8082/spring-crm-rest/api'
 });
